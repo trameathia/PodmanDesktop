cc567dc03e1d30d52403da697a807cad74fff482
Jordan Wyatt <trameathia@gmail.com>
2022-01-06 10:51:42 -0500

    created Image view and viewmodel

---
+++ Jordans Podman Tool/ViewModel/ImageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Jordans_Podman_Tool.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Jordans_Podman_Tool.ViewModel
+{
+    public class ImageViewModel
+    {
+        #region Private Properties
+        private ObservableCollection<Image> images;
+        private DispatcherTimer ImageTimer;
+        #endregion
+        #region Public Properties
+        public ObservableCollection<Image> Images
+        {
+            get => images;
+            set => images = value;
+        }
+        #endregion
+        #region Public Methods
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public ImageViewModel()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            images = new ObservableCollection<Image>();
+
+            PopulateImages();
+            ImageTimer = new DispatcherTimer();
+            ImageTimer.Interval = TimeSpan.FromSeconds(10);
+            ImageTimer.Tick += OnImageEvent;
+            ImageTimer.Start();
+        }
+        #endregion
+        #region Private Methods
+        private void OnImageEvent(object? send, EventArgs e)
+        {
+            PopulateImages();
+        }
+        private void PopulateImages()
+        {
+            string command = "podman image list";
+            if (WSLCommand.Run(command, false, out string output))
+            {
+                Images.Clear();
+                output = output.Substring(output.IndexOf(command) + command.Length + 2);
+                output = output.Substring(output.IndexOf("\n") + 1);
+                if (output.IndexOf("\n\r\n") > -1)
+                {
+                    output = output.Substring(0, output.IndexOf("\n\r\n"));
+                    string[] lines = output.Split("\n");
+                    foreach (string line in lines)
+                    {
+                        string[] split = System.Text.RegularExpressions.Regex.Split(line, @"\s{2,}");
+                        Images.Add(new Image(split[0], split[1], split[2], split[3], split[4]));
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
